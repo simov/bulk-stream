153898000522dff98442f8bc579b4121f9d7743b
simov <simeonvelichkov@gmail.com>
2015-09-08 08:50:55 +0300

    Minor refactoring in tests

--- test/index.js
+++ test/index.js
@@ -8,20 +8,17 @@ var BulkStream = require('../')
 var image0 = path.resolve(__dirname, 'fixtures/cat0.png')
   , image1 = path.resolve(__dirname, 'fixtures/cat1.png')
   , image2 = path.resolve(__dirname, 'fixtures/cat2.png')
-var input0 = fs.createReadStream(image0, {highWaterMark:1024})
-  , input1 = fs.createReadStream(image1, {highWaterMark:1024})
-  , input2 = fs.createReadStream(image2, {highWaterMark:1024})
-var tmp0 = path.resolve(__dirname, 'tmp/cat0.png')
-  , tmp1 = path.resolve(__dirname, 'tmp/cat1.png')
-  , tmp2 = path.resolve(__dirname, 'tmp/cat2.png')
+var tmp = path.resolve(__dirname, 'tmp/cat.png')
 
 
 describe('strings + stream', function () {
   it('0', function (done) {
+    var input = fs.createReadStream(image2, {highWaterMark:1024})
+
     var bs = new BulkStream()
     bs.append('wqw')
     bs.append('\r\n')
-    bs.append(input2)
+    bs.append(input)
     bs.append('\r\n')
     bs.append('poop')
 
@@ -40,8 +37,8 @@ describe('strings + stream', function () {
       data.should.match(/PNG/)
       data.should.match(/poop/)
 
-      fs.writeFileSync(tmp2, buffer.slice())
-      var stats = fs.statSync(tmp2)
+      fs.writeFileSync(tmp, buffer.slice())
+      var stats = fs.statSync(tmp)
       stats.size.should.equal(22025)
       done()
     })
